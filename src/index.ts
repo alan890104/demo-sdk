7a7048488f0cbb7a3f25277a21fcf29118b88996
alan890104 <alan890104@gmail.com>
2024-08-29 23:13:30 +0800

    chore: Update package.json with decimal.js dependency

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import { beginCell } from "@ton/core";
-
+import Decimal from "decimal.js";
 export * from "./moduleA";
 
 export function swap(): string {
+    console.log(new Decimal("1").add(1).toString());
     return beginCell().storeBit(1).storeCoins(10).endCell().toString();
 }
